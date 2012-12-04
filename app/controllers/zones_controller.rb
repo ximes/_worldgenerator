51f0b228f743cba5b3ee6012feac173d1370e1ef
stefano franzin <stefano.franzin@seesaw.it>
2012-12-04 15:02:47 +0100

    new climate generation

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -11,14 +11,19 @@ class ZonesController < ApplicationController
   end
 
   def index
-    @x = params[:x].blank? ? 0 : params[:x]
-    @y = params[:y].blank? ? 0 : params[:y]
-    @z = params[:z].blank? ? 0 : params[:z]
+    @x = params[:x].blank? ? 0 : params[:x].to_i
+    @y = params[:y].blank? ? 0 : params[:y].to_i
+    @z = params[:z].blank? ? 0 : params[:z].to_i
 
+
+
+    #create the zone if doesn't exist
     @zone = Zone.find_or_create_by_x_and_y_and_z(@x, @y, @z)
     @zone.visited=true
     @zone.save
 
+
+    #get the existing climates
     @climates = []
     climates_list = []
     climates=Climate.all()
@@ -26,6 +31,39 @@ class ZonesController < ApplicationController
       climate[:distance] = Math::sqrt((climate.x - @zone.x)**2 + (climate.y - @zone.y)**2)
     end
     climates.keep_if {|v| v[:radius] >= v[:distance] || v[:default]}
+
+
+
+
+    #create the climate if doesn't exist
+
+    if climates.size <= 2
+
+      #check boundaries
+      bound_x=Range.new(Zone.minimum("x").to_i, Zone.maximum("x").to_i)
+      bound_y=Range.new(Zone.minimum("y").to_i, Zone.maximum("y").to_i)
+      bound_z=Range.new(Zone.minimum("z").to_i, Zone.maximum("z").to_i)
+
+      #check directions
+      dir_x = @x.to_i>=0 ? 1 : -1
+      dir_y = @y.to_i>=0 ? 1 : -1
+      dir_z = @z.to_i>=0 ? 1 : -1
+
+    @bound = "s"
+    random_value = ((Random.rand(bound_x) + Random.rand(bound_y) + Random.rand(bound_z))/3).to_i.abs
+      
+    new_climate=Climate.new()
+    new_climate.name = rand(36**16).to_s(36)
+    new_climate.x = @x+dir_x*Random.rand(random_value)
+    new_climate.y = @y+dir_y*Random.rand(random_value)
+    new_climate.z = @z+dir_z*Random.rand(random_value)
+    new_climate.color = "##{Random.rand(255).to_s(16)}#{Random.rand(255).to_s(16)}#{Random.rand(255).to_s(16)}"
+    new_climate.radius = random_value
+    new_climate.scale = Random.rand(100)/10
+    new_climate.save()
+    end
+
+    
 
     climates.sort_by { |h| h[:distance] }
     @climates = climates
