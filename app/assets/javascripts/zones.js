224c5735594a314681554f674ba90df13a17704d
stefano franzin <stefano.franzin@seesaw.it>
2012-12-04 14:16:35 +0100

    randomised rough div order

---
+++ app/assets/javascripts/zones.js
@@ -0,0 +1,7 @@
+$(function () {
+    var parent = $("#zones_container");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+});
